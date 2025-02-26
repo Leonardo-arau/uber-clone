4d6dbdd0c96705998982aabaeac17f8f5f3afc9f
Leonardo-arau <leo-a-oliveira@outlook.com>
2025-02-26 17:12:58 -0300

    Primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:uber/telas/home.dart';
+
+void main() => runApp(MaterialApp(
+      title: "Uber",
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    ));
