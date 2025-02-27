8161f50c99d20295c8eb21bc6b40379ae9e52f87
Leonardo-arau <leo-a-oliveira@outlook.com>
2025-02-27 15:33:07 -0300

    Criando interface de login

--- lib/telas/Home.dart
+++ lib/telas/Home.dart
@@ -1,13 +1,109 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
+  const Home({super.key});
+
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+
+  final TextEditingController _controllerEmail = TextEditingController();
+  final TextEditingController _controllerSenha = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("imagens/fundo1.jpeg"),
+            fit: BoxFit.cover
+          )
+        ),
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(padding: EdgeInsets.only(bottom: 32),
+                child: Image.asset(
+                    "imagens/logo1.png",
+                  width: 700,
+                  height: 300,
+                  ),
+                ),
+                TextField(
+                  controller: _controllerEmail,
+                  autofocus: true,
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                    hintText: "e-mail",
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(6)
+                    ) 
+                  ),
+                ),
+                TextField(
+                  controller: _controllerSenha,
+                  obscureText: true,
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                    hintText: "senha",
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(6)
+                    ) 
+                  ),
+                ),
+                Padding(
+                padding: EdgeInsets.only(top: 16, bottom: 10),
+                child: ElevatedButton(
+                  onPressed: () {}, 
+                  style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xff1ebbd8),
+                padding: EdgeInsets.fromLTRB(32, 16, 32, 16), // Cor do botão corrigida
+                ),
+                child: Text(
+                "Entrar",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                ),
+               ),
+               Center(
+                child: GestureDetector(
+                  child: Text(
+                    "Não tem conta? cadastre-se!",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onTap: (){
+
+                  },
+                ),
+               ),
+               Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Center(
+                  child: Text(
+                    "Error",
+                    style: TextStyle(color: Colors.red, fontSize: 20),
+                    ),
+                ),
+               ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
